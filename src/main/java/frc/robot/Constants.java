0dc94db05d9440cc1070068bff62eadb4b2953f6
GabrielPuyo <93557857+GabrielPuyo@users.noreply.github.com>
2022-03-01 16:41:46 -0500

    Colin: Added What He thinks the control command was (Theres anotherone that it could be) plus added a placeholder constant for the intake motor

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,7 +17,10 @@ public final class Constants {
     TOP_LEFT_DRIVE = 4,
     BOTTOM_LEFT_DRIVE = 1,
     TOP_RIGHT_DRIVE = 3,
-    BOTTOM_RIGHT_DRIVE = 2;
+    BOTTOM_RIGHT_DRIVE = 2,
+    INTAKE_MOTOR = 15;
+    // this int is a placholder for now cuz colin doesn't know which
+    // controller to use or whatever 
 
     public static final int
     SHIFTER_FORWARD = 1,
